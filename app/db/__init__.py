c43a1d9a8c2e86262cba8673fc8745033716db41
Chelsea <chibby@chelseas-mbp.mynetworksettings.com>
2024-07-30 21:13:42 -0400

    Connect Database & Import Filters

--- app/db/__init__.py
+++ app/db/__init__.py
@@ -3,6 +3,7 @@ from sqlalchemy.ext.declarative import declarative_base # type: ignore
 from sqlalchemy import create_engine # type: ignore
 from sqlalchemy.orm import sessionmaker # type: ignore
 from dotenv import load_dotenv # type: ignore
+from flask import g # type: ignore
 
 load_dotenv()
 
@@ -10,3 +11,21 @@ load_dotenv()
 engine = create_engine(getenv('DB_URL'), echo=True, pool_size=20, max_overflow=0)
 Session = sessionmaker(bind=engine)
 Base = declarative_base()
+
+def init_db(app):
+  Base.metadata.create_all(engine)
+
+  app.teardown_appcontext(close_db)
+
+def get_db():
+  if 'db' not in g:
+    # store db connection in app context
+    g.db = Session()
+
+  return g.db
+
+def close_db(e=None):
+  db = g.pop('db', None)
+
+  if db is not None:
+    db.close()
